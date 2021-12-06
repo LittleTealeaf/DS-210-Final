cd29bc4f1f2dcfa56b5959cdcc3c7bb451f4e60e
LittleTealeaf <ThomasKwashnak@gmail.com>
2021-12-06 17:46:31 -0500

    Data now imports as 2 matrices

--- dataset/data_import.py
+++ dataset/data_import.py
@@ -8,6 +8,8 @@ tile_dictionary = {
 Goals of this script:
  - Read data from dataset into two matricies. One matrix contains the list of all inputs and another matrix has the proper outputs (p1 win, draw, p2 win)
 '''
+
+
 def read_file(fileName):
     '''
     Reads the raw connect-4 data and converts it into a single matrix
@@ -20,4 +22,20 @@ def generate_converters(columnCount):
         converters[i] = lambda s: tile_dictionary[s.decode("utf-8")]
     return converters
 
-print(read_file("connect-4-data.csv"))
+def get_data(fileName):
+    '''
+    Returns two matricies, the first being the data, and the second being the expected outputs
+    '''
+    data_full = read_file(fileName)
+
+    data_inputs = data_full[:,:-1]
+
+    data_outputs_partial = data_full[:,-1:]
+    data_outputs = np.zeros((len(data_outputs_partial),3))
+    for i in range(len(data_outputs)):
+        data_outputs[i][int(data_outputs_partial[i][0])] = 1
+    
+    return data_inputs, data_outputs
+
+
+print(get_data("connect-4-data.csv"))
