0d0c3601e03a1625cac74ac711776ffa9e11f942
LittleTealeaf <ThomasKwashnak@gmail.com>
2021-12-06 17:51:26 -0500

    Changes

--- dataset/data_import.py
+++ dataset/data_import.py
@@ -4,12 +4,6 @@ tile_dictionary = {
     "b": 0, "x": 1, "o": -1, "draw": 0, "win": 1, "loss": -1
 }
 
-'''
-Goals of this script:
- - Read data from dataset into two matricies. One matrix contains the list of all inputs and another matrix has the proper outputs (p1 win, draw, p2 win)
-'''
-
-
 def read_file(fileName):
     '''
     Reads the raw connect-4 data and converts it into a single matrix
@@ -17,6 +11,9 @@ def read_file(fileName):
     return np.loadtxt(fileName,dtype=float,delimiter=",",converters=generate_converters(43))
 
 def generate_converters(columnCount):
+    '''
+    Creates a generator that assigns each column number to use a lambda function that searches the string up on tile_dictionary
+    '''
     converters = {}
     for i in range(columnCount):
         converters[i] = lambda s: tile_dictionary[s.decode("utf-8")]
