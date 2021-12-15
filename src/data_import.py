93757574a62a0ca4619f0ea3f14fc896c42c8502
LittleTealeaf <ThomasKwashnak@gmail.com>
2021-12-15 13:05:48 -0500

    Fixed stuff, started working on code documentation

--- src/data_import.py
+++ src/data_import.py
@@ -1,48 +1,67 @@
 import numpy as np
 
-# TODO: proper comments
 
-def read_file(fileName):
-    '''
-    Reads the raw connect-4 data and returns it as a single matrix (excluding the first row)
-    
-    Inputs:
-     - fileName: file name (path) of .csv data
-    Output: 2-dimensional matrix of data extracted from the CSV file, using generate_converters to convert letters into numbers
-    '''
+def read_file(fileName: str):
+    """Reads the raw connect-4 data from a file and converts it into a single matrix. Excludes the column headers (the first row) from reading
+
+    Input:
+    - fileName: Name of the file to read
+
+    Output:
+    - A matrix containing the raw data from the file.
+    """
     return np.loadtxt(fileName,dtype=float,delimiter=",",converters=generate_converters(43),skiprows=1)
 
-def generate_converters(columnCount):
-    '''
-    Creates a generator that assigns each column number to use a lambda function that searches the string up on tile_dictionary
+def generate_converters(columnCount: int = 43):
+    """Creates a generator that assigns each column number to use a lambda function that replaces the string with an associated number.
     
-    Input: number of columns to use the converter on
-    Output: map from all specified columns to the respective dictionary to convert from raw data to numerical data
-    '''
+    Input:
+    - columCount (default = 43): The number of columns to use the converter on. With the connect-4 dataset, there are 43 columns which should be converted.
+    
+    Output:
+    - Dictionary that associates each column provided to a lambda converter that looks up the string on the dictionary
+    """
     converters = {}
 
     tile_dictionary = {
         "b": 0, "x": 1, "o": -1, "draw": 0, "win": 1, "loss": 0
     }
 
+    converter = lambda s: tile_dictionary[s.decode("utf-8")]
+
     for i in range(columnCount):
-        converters[i] = lambda s: tile_dictionary[s.decode("utf-8")]
+        converters[i] = converter
     return converters
 
-def get_data(fileName):
-    '''
-    Returns two matricies, the first being the data, and the second being the expected outputs
 
-    '''
-    data_full = read_file(fileName)
 
-    data_inputs = data_full[:,:-1]
-    data_outputs = data_full[:,-1:]
-
-    return data_inputs, np.transpose(data_outputs)[0]
+def get_data(fileName: str):
+    """Reads data from a file, separating it into an input matrix and an output matrix.
+    
+    Input:
+    - fileName: Name of the file to read.
+    
+    Outputs:
+    - The input matrix, where each row contains the input data for that entry
+    - The output matrix, where each entry returns the output vector expected for the corresponding input vector
+    """
+    data_matrix = read_file(fileName)
+    return [np.array([[j] for j in i]) for i in data_matrix[:,:-1]], [np.array([[j] for j in i]) for i in data_matrix[:,-1:]]
 
 def get_data_full():
+    """Returns data from the full connect-4 data file, separating it into an input matrix and an output matrix.
+
+    Outputs:
+    - The input matrix, where each row contains the input data for that entry
+    - The output matrix, where each entry returns the output vector expected for the corresponding input vector
+    """
     return get_data("resources/connect-4-data.csv")
 
 def get_data_short():
+    """Returns data from the short connect-4 data file, separating it into an input matrix and an output matrix. The short conect-4 data is used for quick verification as it only contains a small list of entries.
+
+    Outputs:
+    - The input matrix, where each row contains the input data for that entry
+    - The output matrix, where each entry returns the output vector expected for the corresponding input vector
+    """
     return get_data("resources/connect-4-data-short.csv")
--- src/lab.py
+++ src/lab.py
@@ -1,4 +1,3 @@
-from typing import Iterator
 import numpy as np
 import math
 import random
@@ -29,7 +28,20 @@ def sigmoid_derivative(n):
     """
     return sigmoid(n) * (1 - sigmoid(n))
 
-def back_propagation(Y2, W1, W0, y_expected):
+def back_propagation(Y2: np.matrix, W1: np.matrix, W0: np.matrix, y_expected: np.double):
+    """Back Propagation for a 2-layer neural network
+    
+    Inputs:
+    - Y2: The input vector (matrix), dimensions m x 1
+    - W1: The weight matrix, dimensions h x m, that represents the weights used as values go from Y2 to Y1
+    - W0: The weight matrix, dimensions 1 x h, that represents the weights used as values go from Y1 to Y0
+    - y_expected: The expected result from Forward-Feeding
+
+    Outputs:
+    - A matrix of the derivatives of the weights in W1, as a h x m matrix
+    - A matrix of the derivatives of the weights in W0, as a 1 x h matrix
+    - The absolute error of the network for the given input
+    """
     dW1 = np.zeros(W1.shape)
     dW0 = np.zeros(W0.shape)
     Y1 = sigmoid(W1 @ Y2)
@@ -41,7 +53,21 @@ def back_propagation(Y2, W1, W0, y_expected):
             dW1[i,j] = dW0[0,i] * W0[0,i] * (1 - Y1[i,0]) * Y2[j,0]
     return dW1,dW0,Err
 
-def stochastic_descent(inputs,outputs,W1,W0,step):
+def stochastic_descent(inputs: list,outputs: list,W1: np.matrix,W0: np.matrix,step: np.double):
+    """Stochastic Gradient Descent Iteration
+    
+    Inputs:
+    - inputs: length n list of input vectors, as matrices. Each input vector is of size m x 1
+    - output: length n list of all expected values, as scalars. Each output corresponsds to the input at the same index.
+    - W1: The weight matrix, dimensions h x m, that represents the weights used as values go from Y2 to Y1
+    - W0: The weight matrix, dimensions 1 x h, that represents the weights used as values go from Y1 to Y2
+    - step: The step coefficient to nudge the weights by
+    
+    Outputs:
+    - The updated weight matrix W1, of dimensions h x m, where values have been modified by the average derivative across all inputs/outputs scaled by the step
+    - The updated weight matrix W0, of dimensions 1 x h, where values have been modified by the average derivative across all inputs/outputs scaled by the step
+    - The average absolute error of the network on each input
+    """
     Err_total = 0
     DW1 = np.zeros(W1.shape)
     DW0 = np.zeros(W0.shape)
@@ -57,13 +83,30 @@ def stochastic_descent(inputs,outputs,W1,W0,step):
     W0 = W0 - DW0 * step / len(inputs)
     return W1, W0, Err_total
 
-def train_network(inputs,outputs,W1_in,W0_in,f_step,n,c,seed):
+def train_network(inputs,outputs,W1_in,W0_in,f_step,iteration_count,batch_size,seed):
+    """Trains the neural network on a set of inputs, outputs, and values.
+
+    Inputs:
+    - inputs: length n list of all input vectors, as matrices. Each input vector is of size m x 1
+    - outputs: length n list of all expected values, as scalars. Each output corresponds to the input at that same index.
+    - W1_in: The weight matrix, of dimensions h x m, that represents the weights used as values go from Y2 to Y1.
+    - W0_in: The weight matrix, of dimensions 1 x h, that represents teh weights used as values go from Y1 to Y0.
+    - f_step: A lambda function that correlates a given iteration i to a step value.
+    - iteration_count: The number of iterations to run the network on.
+    - batch_size: The number of entries to batch together for each iteration
+    - seed: The seed to base the randomization off of
+
+    Outputs:
+    - The updated weight matrix W1, dimensions h x m, trained on the given inputs and outputs.
+    - The updated weight matrix W0, dimensions 1 x h, trained on the given inputs and outputs
+    
+    """
     W1 = W1_in
     W0 = W0_in
     random.seed(seed)
-    for i in range(n):
-        step = f_step(i,n)
-        indexes = [random.randint(0,len(inputs)-1) for j in range(c)]
+    for i in range(iteration_count):
+        step = f_step(i)
+        indexes = [random.randint(0,len(inputs)-1) for j in range(batch_size)]
         input_iteration = [inputs[j] for j in indexes]
         output_iteration = [outputs[j] for j in indexes]
         W1,W0,Err = stochastic_descent(input_iteration,output_iteration,W1,W0,step)
@@ -75,7 +118,7 @@ def evaluate_network(inputs,outputs,W1,W0):
     for i in range(len(inputs)):
         Y0 = sigmoid(W0 @ sigmoid(W1 @ inputs[i]))
         total_error = abs(Y0[0,0] - outputs[i])
-    return total_error / len(inputs)
+    return total_error[0,0] / len(inputs)
 
 def random_matrix(size):
     m = np.zeros(size)
@@ -99,14 +142,13 @@ class Experiment:
 
     def get_evaluation(self):
         if(self.evaluation == -1):
-            W1,W0 = train_network(self.inputs,self.outputs,self.W1,self.W0,lambda n,i: 1.0 - n / i, self.iterations, self.batch_size, self.seed)
+            W1,W0 = train_network(self.inputs,self.outputs,self.W1,self.W0,lambda n: 1.0 - n / self.iterations, self.iterations, self.batch_size, self.seed)
             self.evaluation = evaluate_network(self.inputs, self.outputs, W1, W0)
         return self.evaluation
 
 
 if __name__ == "__main__":
     in_data, out_data = get_data_full()
-    in_data = [np.array([[j] for j in i]) for i in in_data]
     
     seed = 31415926
 
@@ -124,18 +166,4 @@ if __name__ == "__main__":
                     best = a
             
     print(a.hidden_count, a.iterations, a.batch_size)
-
-    # hidden_count = 25
-    # iterations = 10000
-    # batch_size = 5
-    # seed = 32156
-
-
-    # W1 = random_matrix((hidden_count,42))
-    # W0 = random_matrix((1,hidden_count))
-    # W1,W0 = train_network(in_data,out_data,W1,W0,lambda n,i: 1.0 - n / i, iterations,batch_size,seed)
-    # # print(W1)
-    # # print(W0)
-    # print("Evlatuation:",evaluate_network(in_data,out_data,W1,W0))
-
 
