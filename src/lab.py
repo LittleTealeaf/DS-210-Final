9415fde0fdd18d76ecbcca2f4a817c3b2585d5f4
LittleTealeaf <ThomasKwashnak@gmail.com>
2021-12-15 08:47:25 -0500

    Adding python documentation

--- src/lab.py
+++ src/lab.py
@@ -1,18 +1,16 @@
 import numpy as np
-from data_import import *
 import math
 import random
-from functools import lru_cache
+from data_import import *
 
 
 def sigmoid(n):
-    '''
-    Input: A matrix, or a number, `n`
-    
-    Output: `signmoid(n)`. If `n` was a matrix, then will return a 
-    new matrix of identical dimensions where each value is put into 
-    the sigmoid function
-    '''
+    """Calculate the sigmoid function of a given input.
+    Input:
+        n - Either a scalar value, or a single or multidimensional array
+    Output:
+        m - The value of n passed through the sigmoid function. If n is an array, then each value within n is passed through the sigmoid function
+    """
     if isinstance(n,np.ndarray):
         a = np.zeros(n.shape)
         for i in range(len(a)):
@@ -21,14 +19,28 @@ def sigmoid(n):
     else:
         return 1 / (1 + math.exp(-n))
 
-def sigmoidDerivative(n):
-    '''
-    Input: A number `n`
-    Output: The derivative of `sigmoid(n)` at that point
-    '''
+def sigmoid_derivative(n):
+    """Calculates the derivative of the sigmoid function at a given input.
+    Input:
+        n - Either a scalar value, or a single or multidimensional array
+    Output:
+        m - The value of n passed through the derivative of the sigmoid function. If n is an array, then each value within n is passed through the derivative of the sigmoid function
+    """
     return sigmoid(n) * (1 - sigmoid(n))
 
 def backwards_propagation(Y2,W1_in,W0_in,S,y_actual):
+    """Calculates and returns updated weights after updating to minimize the loss function from a given input.
+    Inputs:
+        Y2 - The input vector, dimensions m x 1
+        W1_in - The weight matrix, dimensions h x m, that represent the weights applied from the input layer to the 1st layer
+        W0_in - The weight matrix, dimensions 1 x h, that represent the weights applied from the 1st layer to the 0st layer (output)
+        S - The step coefficient, as a scalar, to modify how much change is made to the weights
+        y_actual - The expected value of the neural network, as a scalar value between [0,1]
+    Outputs:
+        W^1 - The updated weights, dimensions h x m, that represent the weights applied from the input layer to the 1st layer
+        W^0 - The updated weights, dimensions h x m, that represent the weights applied from the 1st layer to the 0st layer (output)
+        Err - The absolute error, as a scalar value, of the predicted output and the actual output for the given input
+    """
     W1 = np.copy(W1_in)
     W0 = np.copy(W0_in)
     Y1 = sigmoid(W1 @ Y2)
